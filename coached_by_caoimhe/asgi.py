d6225fdeec16eb53d5dc9c5e498f68328643c6c4
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-12-10 17:49:45 +0000

    Initial commit

---
+++ coached_by_caoimhe/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for coached_by_caoimhe project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coached_by_caoimhe.settings')
+
+application = get_asgi_application()
