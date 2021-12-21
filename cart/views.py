a8ffffa66fba74ccab9b4e27b0763e96efec53ed
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-12-21 19:21:56 +0000

    Add remove_from_bag view and complete the remove plan button functionality

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 
 
@@ -26,3 +27,21 @@ def add_to_cart(request, item_id):
     request.session["cart"] = cart
     # Redirect user to the page they were on
     return redirect(redirect_url)
+
+def remove_from_cart(request, item_id):
+    """Remove a specified product from the cart"""
+    
+    try:
+        # Store session key in a variable to allow user to move through site without losing session
+        cart = request.session.get("cart", {})
+
+        # If item is in cart, remove it from cart
+        if item_id in list(cart.keys()):
+            cart.pop(item_id)
+
+        # Update session cart
+        request.session["cart"] = cart
+        # Redirect user to the page they were on
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(f"Error: {e}", status=500)
