7e5599e7a1926360c0ab7fb60346c0005873b786
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-12-22 16:32:59 +0000

    Ädd notification css, cart preview

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,4 @@
-from django.http.response import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404, HttpResponse
 from django.contrib import messages
 
 from workouts.models import Workout
@@ -15,16 +14,16 @@ def add_to_cart(request, item_id):
     """Add a specified product to the cart"""
 
     # Get workout from database
-    workout = Workout.objects.get(pk=item_id)
+    workout = get_object_or_404(Workout, pk=item_id)
     # Get redirect url so user can be redirected to the page they were on
     redirect_url = request.POST.get("redirect_url")
     # Store session key in a variable to allow user to move through site without losing session
     cart = request.session.get("cart", {})
-
     # If item is already in cart, do not increase quantity by 1
     if item_id in list(cart.keys()):
         cart[item_id] = 1
-        messages.success(request, f"{workout.name} has been added to your cart!")
+        # item_id already in cart, show message
+        messages.info(request, f"{workout.name} has already been added to your cart!")
     # If item is not in cart, add it to cart
     else:
         cart[item_id] = 1
@@ -35,9 +34,13 @@ def add_to_cart(request, item_id):
     # Redirect user to the page they were on
     return redirect(redirect_url)
 
+
 def remove_from_cart(request, item_id):
     """Remove a specified product from the cart"""
-    
+
+    # Get workout from database
+    workout = get_object_or_404(Workout, pk=item_id)
+
     try:
         # Store session key in a variable to allow user to move through site without losing session
         cart = request.session.get("cart", {})
@@ -45,10 +48,14 @@ def remove_from_cart(request, item_id):
         # If item is in cart, remove it from cart
         if item_id in list(cart.keys()):
             cart.pop(item_id)
+            messages.success(
+                request, f"{workout.name} has been removed from your cart!"
+            )
 
         # Update session cart
         request.session["cart"] = cart
         # Redirect user to the page they were on
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f"Error removing item: {e}")
         return HttpResponse(f"Error: {e}", status=500)
