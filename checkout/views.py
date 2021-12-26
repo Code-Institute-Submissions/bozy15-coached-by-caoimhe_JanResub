e71c43ea45c7045a3f63a931d4511af1c1f133f7
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-12-26 11:24:21 +0000

    Add checkout form cache in payment intent and fixed issue wirh checkout.js
    There was an issue with submit the payment for being submitted

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
+from django.views.decorators.http import require_POST
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -9,6 +11,27 @@ from cart.contexts import cart_contents
 
 import stripe
 import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    """ Captures data from stripe and saves it in PaymentIntent """
+
+    try:
+        # Get the payment intent id
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        # Modify the payment intent
+        stripe.PaymentIntent.modify(pid, metadata={
+            "cart": json.dumps(request.session.get('cart', {})),
+            "save_info": request.POST.get('save_info'),
+            "username": request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, "Sorry, your payment cannot be \
+            processed right now. Please try again later.")
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
