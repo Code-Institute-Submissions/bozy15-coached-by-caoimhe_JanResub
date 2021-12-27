bfbdb481a707d39db75e7c35c7e5e39d2e380132
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-12-27 21:53:51 +0000

    Deployed app to heroku

--- coached_by_caoimhe/settings.py
+++ coached_by_caoimhe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -51,7 +52,6 @@ INSTALLED_APPS = [
     "cart",
     "checkout",
     "profiles",
-
     # "django_extensions",
     "crispy_forms",
     "crispy_bootstrap5",
@@ -90,11 +90,11 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "django.template.context_processors.media",
-                "cart.contexts.cart_contents", # Required to allow access to all templates
+                "cart.contexts.cart_contents",  # Required to allow access to all templates
             ],
-            "builtins": [ 
-                'crispy_forms.templatetags.crispy_forms_tags',
-                'crispy_forms.templatetags.crispy_forms_field',
+            "builtins": [
+                "crispy_forms.templatetags.crispy_forms_tags",
+                "crispy_forms.templatetags.crispy_forms_field",
             ],
         },
     },
@@ -184,7 +184,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
