47c0bd5f0db04a06962373a7b1607b8e951180de
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-12-27 18:57:28 +0000

    Add url for edit_workout view and updated header on edit_workout.html

--- workouts/urls.py
+++ workouts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.all_workouts, name="workouts"), # path to all workouts
     path("<int:workout_id>/", views.workout_detail, name="workout_detail"), # path to workout detail
     path("add/", views.add_workout, name="add_workout"), # path to add workout
+    path("edit/<int:workout_id>", views.edit_workout, name="edit_workout"), # path to add workout
 ]
