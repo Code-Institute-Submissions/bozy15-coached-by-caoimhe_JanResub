7794ca063b0150ee0e48901a2a04e43d1284bb74
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-12-27 00:55:34 +0000

    Updated webhook handler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,6 +7,7 @@ from django.conf import settings
 
 from .models import Order, OrderLineItem
 from workouts.models import Workout
+from profiles.models import UserProfile
 
 import json
 import time
@@ -66,6 +67,7 @@ class StripeWH_Handler:
         if username != "AnonymousUser":
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
+                profile.default_full_name = shipping_details.name
                 profile.default_phone_number = shipping_details.phone
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
