375a85c533189fbeb4431af4cca0dbd09450fc9f
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-12-28 00:27:02 +0000

    Changed allowed hosts

--- coached_by_caoimhe/settings.py
+++ coached_by_caoimhe/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ["https://coached-by-caoimhe.herokuapp.com/", "http://127.0.0.1:8000"]
+ALLOWED_HOSTS = ["coached-by-caoimhe.herokuapp.com/", "http://127.0.0.1:8000"]
 
 
 # Application definition
