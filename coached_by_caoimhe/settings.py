5cb3563ca61dbf01f0e63742b5a396f7cf47da12
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-12-28 00:57:35 +0000

    change develop to true

--- coached_by_caoimhe/settings.py
+++ coached_by_caoimhe/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "DEVELOPMENT" in os.environ
+DEBUG = True#"DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ["coached-by-caoimhe.herokuapp.com", "127.0.0.1"]
 
