7f3529f8bd94bd654abc19a495f6e502d9f4e270
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2022-01-13 19:04:05 +0000

    Add automatic email functionality and check if email exists

--- subscribe/views.py
+++ subscribe/views.py
@@ -16,15 +16,32 @@ def subscribe_form(request):
 
     # If the form is submitted
     if request.method == "POST":
-        # create an instance of the form
+        # Create an instance of the form
         form = SubscribeForm(request.POST)
+        # Get all email values from database
+        email_list = Subscribe.objects.values_list("email", flat=True)
+        # Check if the email is already in the database
+        if request.POST["email"] in email_list:
+            messages.error(request, "You are already subscribed to our newsletter")
+            return render(request, "home/index.html", {"form": form})
         # check if the form is valid
-        if form.is_valid():
+        elif form.is_valid():
             # Save the form data to the database
             user_email = form.cleaned_data.get("email")
             print(user_email)
             form.save()
             form = SubscribeForm()
+            # Send email to the user
+            try:
+                send_mail(
+                    "Coached By Caoimhe email subscription",
+                    "Thank you for subscribing to our newsletter",
+                    settings.EMAIL_HOST_USER,
+                    [user_email],
+                    fail_silently=False,  # If the email fails to send, it will raise an error
+                )
+            except BadHeaderError:
+                return HttpResponse("Invalid header found")
             # display toast
             messages.success(request, "Thank you for subscribing!")
         else:
